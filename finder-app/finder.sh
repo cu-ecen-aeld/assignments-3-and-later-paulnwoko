ad1f7b257d359df13be1e61fd3b31acc48e4d515
Paul Nwoko <npaul511@gmail.com>
2024-05-15 02:12:49 +0100

    add lines to count number of file and number of string occurance in a recursive file search

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -13,9 +13,15 @@ if [ ${NUM_OF_ARG} -eq 2 ]
 then
    #check if dir entered is truly a dir
    if [ -d ${FILESDIR} ]
-   then
-      #perform the search here
-      find . -${SEARCHSTR} ${FILESDIR}
+   then    
+      #count files in dir
+      #NO_OF_FILES=$(ls -A | wc -l)
+      #count files in dir and subdir
+      NO_OF_FILES=$(find ${FILESDIR} -type f -name "*" | wc -l)
+      #enter target dir
+      cd ${FILESDIR}
+      #perform a recursive search for matching string in the dir
+      NO_OF_MATCHING_LINES=$(grep -r -o ${SEARCHSTR} * | wc -l)
       echo "The number of files are ${NO_OF_FILES} and the number of matching lines are ${NO_OF_MATCHING_LINES}"
       #echo "Is a DIR = ${FILESDIR} and str = ${SEARCHSTR} ARG = ${NUM_OF_ARG}"
       exit 1
