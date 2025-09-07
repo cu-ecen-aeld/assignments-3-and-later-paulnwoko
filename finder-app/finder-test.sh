f1ca8c17fb253500d44e04b05e91ee561639a6a8
Paul Nwoko <npail511@gmail.com>
2025-09-07 12:24:32 +0100

    Modify your finder-test.sh to run with necessary files found in the PATH and to write a file with output of the finder command to /tmp/assignment4-result.txt

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,11 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+#username=$(cat conf/username.txt)
+
+# Config files are expected under /etc/finder-app/conf - for assignment4-2
+username=$(cat /etc/finder-app/conf/username.txt)
+
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +36,10 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt` #assignment=`cat ../conf/assignment.txt`
+#assignment=`cat conf/assignment.txt` #assignment=`cat ../conf/assignment.txt`
+
+# Config files are expected under /etc/finder-app/conf for assignment4-2
+assignment=$(cat /etc/finder-app/conf/assignment.txt)
 
 if [ $assignment != 'assignment1' ]
 then
@@ -58,6 +65,9 @@ do
 done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+
+# Write the output to /tmp/assignment4-result.txt - for assignment 4-2
+echo "${OUTPUTSTRING}" > /tmp/assignment4-result.txt
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
